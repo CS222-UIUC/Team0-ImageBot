560a433516d02c91f5154118bde580cf85cc8b52
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2023-02-21 17:50:25 -0600

    style: moved bot initialization to main function

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ import discord
 
 import os
 
-#heroku - get token from environment variable
-if "IMAGE_BOT_TOKEN" in os.environ:
-    BOT_TOKEN = os.environ["IMAGE_BOT_TOKEN"]
-#local dev - get token from BOT_TOKEN.txt
-elif os.path.exists("./BOT_TOKEN.txt"):
-    with open("BOT_TOKEN.txt", "r") as token_file:
-        BOT_TOKEN = token_file.read()
-else:
-    raise RuntimeError("Could not find token")
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -24,10 +13,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    #necessary so the bot doesn't respond to itself
     if message.author == client.user:
         return
 
+    #can generally create 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(BOT_TOKEN)
+if __name__ == "__main__":
+    #heroku - get token from environment variable
+    if "IMAGE_BOT_TOKEN" in os.environ:
+        BOT_TOKEN = os.environ["IMAGE_BOT_TOKEN"]
+    #local dev - get token from BOT_TOKEN.txt
+    elif os.path.exists("./BOT_TOKEN.txt"):
+        with open("BOT_TOKEN.txt", "r") as token_file:
+            BOT_TOKEN = token_file.read()
+    else:
+        raise RuntimeError("Could not find token")
+
+    client.run(BOT_TOKEN)
