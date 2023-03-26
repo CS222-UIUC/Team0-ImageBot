6dfff20a6dffdb571a89e94580ca6dc6bf76bd6b
dsding2 <dsding2@illinois.edu>
2023-03-26 14:21:39 -0500

    small documentation update to reflect changes

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ Basic func example, does nothing to the image
 async def test_image_fun(img_path):
     return
 
-@bot.command(name="test_image", description="Test that the bot can download images and send them back")
-async def test_image(ctx, *args):
+@bot.command(name="echo", description="Echos back the image")
+async def echo(ctx, *args):
     await process_command(ctx, test_image_fun, *args)
 
-@test_image.error
+@echo.error
 async def image_error_handler(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please send a URL linking to your image")
