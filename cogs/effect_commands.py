d7e8cf7619863ca88a0eb9df94ecd60a86f8fee8
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2023-04-08 21:01:16 -0500

    feat: animate triangulation effect

--- cogs/effect_commands.py
+++ cogs/effect_commands.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from discord.ext.commands import MissingRequiredArgument, TooManyArguments, BadArgument, CommandInvokeError
 
-from utils.effect import Triangulate
+from utils.effect import Triangulate, TriAnimation
 from image_utils import process_command, InvalidURL
 
 class EffectCog(commands.Cog):
@@ -21,5 +21,18 @@ class EffectCog(commands.Cog):
         else:
             await ctx.send(f"Something unexpected happened {error}")
 
+    @commands.command(name="tri_animate", description="create an animated triangulation of an image")
+    async def tri_animation(self, ctx, *args):
+        await process_command(ctx, TriAnimation(), *args)
+
+    @tri_animation.error
+    async def tri_animate_error_handler(self, ctx, error):
+        if isinstance(error, (MissingRequiredArgument, TooManyArguments)):
+            await ctx.send(f"Usage: {Triangulate().usage}")
+        elif isinstance(error, BadArgument):
+            await ctx.send(error)
+        else:
+            await ctx.send(f"Something unexpected happened {error}")
+            
 async def setup(bot):
     await bot.add_cog(EffectCog(bot))
--- utils/effect.py
+++ utils/effect.py
@@ -1,26 +1,63 @@
-from discord.ext.commands import BadArgument
+import os
+import shutil
+from PIL import Image
 
+from discord.ext.commands import BadArgument
 from triangler import *
 
 from command import Command
 
-class Triangulate(Command):
-    MIN_POINTS = 1
-    MAX_POINTS = 16383
+MIN_POINTS = 1
+MAX_POINTS = 16383
 
+async def triangulate_image(in_path, out_path, points: int):
+    if points > MAX_POINTS or points < MIN_POINTS:
+        raise BadArgument(f"Number of points must be in range [{MIN_POINTS}, {MAX_POINTS}]")
+    triangler_instance = Triangler(
+        edge_method=EdgeMethod.SOBEL,
+        sample_method=SampleMethod.POISSON_DISK,
+        color_method=ColorMethod.CENTROID,
+        points=points,
+        blur=2,
+        pyramid_reduce=True,
+    )
+    triangler_instance.convert_and_save(in_path, out_path)
+
+class Triangulate(Command):
     def __init__(self):
         super().__init__("$triangulate [points] [image link/uploaded image]")
     
     async def command(self, img_path, points: int):
-        if points > self.MAX_POINTS or points < self.MIN_POINTS:
-            raise BadArgument(f"Number of points must be in range [{self.MIN_POINTS}, {self.MAX_POINTS}]")
-        triangler_instance = Triangler(
-            edge_method=EdgeMethod.SOBEL,
-            sample_method=SampleMethod.POISSON_DISK,
-            color_method=ColorMethod.CENTROID,
-            points=points,
-            blur=2,
-            pyramid_reduce=True,
-        )
-        triangler_instance.convert_and_save(img_path, img_path)
+        await triangulate_image(img_path, img_path, points=points)
         return img_path
+
+class TriAnimation(Command):
+    def __init__(self):
+        super().__init__("$triangulate_animate [image link/uploaded image")
+    
+    async def command(self, img_path):
+        #setup output directory
+        img_dir = os.path.dirname(img_path)
+        out_dir = os.path.join(img_dir, "temp")
+        os.makedirs(out_dir)
+
+        #create frames
+        frames = []
+        #TODO: better point function
+        #see if we can keep the same seed?
+        for p in range(10, 2000, 50): 
+            #setup output name
+            base_name = os.path.basename(img_path)
+            _, extension = os.path.splitext(img_path)
+            out_path = os.path.join(out_dir, f"{base_name}_{p:04d}.{extension}")
+            
+            await triangulate_image(img_path, out_path, p)
+            frames.append(Image.open(out_path))
+
+        #create gif
+        output_file = os.path.join(img_dir, "output.gif")
+        frames[0].save(output_file, save_all=True, append_images=frames[1:], duration=100, loop=0)
+
+        #delete output dir
+        shutil.rmtree(out_dir)
+        return output_file
