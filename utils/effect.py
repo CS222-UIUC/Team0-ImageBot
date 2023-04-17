f6a02c62d9fc6b22b6e3d2c54290633c71fe219a
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2023-04-16 19:42:19 -0500

    feat: voronoi image function

--- utils/effect.py
+++ utils/effect.py
@@ -3,6 +3,9 @@ import os
 import shutil
 from PIL import Image
 import cv2
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
 
 from discord.ext.commands import BadArgument
 from triangler import *
@@ -88,3 +91,29 @@ class TriAnimation(Command):
         #delete output dir
         shutil.rmtree(out_dir)
         return output_file
+
+async def voronoi_image(in_path, out_path, num_points: int, seed: int):
+    if num_points < 4:
+        raise BadArgument("Points must be in range [4, x]")
+
+    np.random.seed(seed)
+    img = Image.open(in_path)
+    
+    np_img = np.array(img)
+
+    height = np_img.shape[0]
+    width = np_img.shape[1]
+
+    points = np.random.rand(num_points, 2) * [width, height]
+
+    rgb = np.zeros((num_points, 3), dtype=np.uint8)
+    for i in range(num_points):
+        p = points[i]
+        x, y = int(p[0]), int(p[1])
+        rgb[i, :] = np_img[x, y, :3]
+
+    grid_x, grid_y = np.mgrid[0:width, 0:height]
+    labels = griddata(points, np.arange(num_points), (grid_x, grid_y), method='nearest')
+    rgb_labels = rgb[labels]
+    plt.imsave(out_path, rgb_labels)
+
